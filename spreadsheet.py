f97e302a20be8a30ee930a428f6db23e8d7a40d9
Kien Ha <kienha9922@gmail.com>
2018-02-22 02:49:17 -0500

    Implement ability to get heroes stats
    Returns a dictionary of heroes stats
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

---
+++ spreadsheet.py
@@ -0,0 +1,31 @@
+import gspread
+import json
+import pprint
+
+from oauth2client.service_account import ServiceAccountCredentials
+
+def main():
+    heroes_data = get_heroes_stats()
+
+def get_heroes_stats():
+    json_data = open('stats.json').read()
+    data = json.loads(json_data)
+    heroes_data = data['heroes']
+    dict_data = {element['name']:element for element in heroes_data}
+    return dict_data
+
+def spreadsheet_work():
+    scope = ['https://spreadsheets.google.com/feeds']
+    #scope_string = ' '.join(scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('Fire-Emblem-Heroes-2918b0c607c7.json', scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open('Copy of Fire Emblem Heroes Bias Spreadsheet').sheet1
+
+    pp = pprint.PrettyPrinter()
+    row = ["5", "Test", "1", "2", "3", "4", "5", "6", "7"]
+    index = 6
+    sheet.insert_row(row, index)
+
+if __name__ == '__main__':
+    main()

9df430ff188e4412c2cda12fe5aa76aae648c5e8
Kien Ha <kienha9922@gmail.com>
2018-02-28 17:24:13 -0500

    Fix bug with last element of row was being replaced with previous entry
    The last entry being entered into the spreadsheet was being replaced by
    its previous row entry, causing duplicates and possible missing entries
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

--- reorganize.py
+++ reorganize.py
@@ -6,8 +6,15 @@ pp = pprint.PrettyPrinter()
 spreadsheet_object = spreadsheet.Spreadsheet("stats.json")
 data = spreadsheet_object.sort_hero_data()
 cell_data = []
-y_range = 0
+
+"""
+Start at y_range = 1 (and not 0) because spreadsheet row 1 is
+the titles. +1 to account for this and place all hero data
+below row 1.
+"""
+y_range = 1
 for hero in data:
     cell_data.append(list(hero.values()))
     y_range += 1
+
 spreadsheet_object.reorganize_spreadsheet(cell_data, y_range)

4acde68a967c2c37685f3ff123a5da23ccebb7d8
Kien Ha <kienha9922@gmail.com>
2018-03-03 20:22:44 -0500

    Add skeleton structure of what scraping data should look like
    End result of scraping data on one page of FEH wiki.
    
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import pprint
+import json
 
 quote_page = 'https://feheroes.gamepedia.com/Abel'
 page = urllib.request.urlopen(quote_page)
@@ -9,5 +10,18 @@ soup = BeautifulSoup(page, 'html.parser')
 tables = soup.find_all('table', {'class': 'wikitable default'})
 table = tables[1]
 td = table.find_all('td')
-for element in td:
-    print(element.text)
+
+data = []
+for stat in td:
+    data.append(stat.text)
+
+dict_data = {
+    'rarity': data[7],
+    'hp': data[8],
+    'atk': data[9],
+    'spd': data[10],
+    'def': data[11],
+    'res': data[12]
+}
+
+print(dict_data)
